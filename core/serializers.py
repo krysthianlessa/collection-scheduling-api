2433061c08c6dd62490cb55e48d8dc9ae9ebe8ab
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-04-25 00:10:49 -0300

    feat: add creaed_by and shared_calendar in ScheduleSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import Schedule
+from core.models import Schedule, SharedCalendar
 
 
 class ScheduleSerializer(ModelSerializer):
     class Meta:
         model = Schedule
+        fields = '__all__'
+
+    def create(self, validated_data):
+        # Get the user making the request
+        user = self.context['request'].user
+        
+        # Set the created_by field to the user
+        validated_data['created_by'] = user
+        
+        # Query for an active SharedCalendar associated with the user
+        shared_calendar = SharedCalendar.objects.filter(user=user, is_active=True).first()
+        
+        # If a valid SharedCalendar is found, set it as the shared_calendar field
+        if shared_calendar:
+            validated_data['shared_calendar'] = shared_calendar
+
+        # Create the Schedule instance
+        schedule = Schedule.objects.create(**validated_data)
+        return schedule
