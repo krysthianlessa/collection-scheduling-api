8219088f80ceed7c78702f5d021b8fd28c587a19
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-05-03 17:26:43 -0300

    feat: refactor pooling to use class based. Add new value in .env.example

--- scripts/pooling.py
+++ scripts/pooling.py
@@ -8,56 +8,56 @@ LAB_ZAP_URL = "http://labzap:3000/"
 RECIPIENT_PHONE_NUMBER = os.environ.get("RECIPIENT_PHONE_NUMBER", default="123")
 BACKEND_API_KEY = os.environ.get("BACKEND_API_KEY", default="XIYsieyz.pyysqzZIRn6GNjCxGPFjptO1nf2SA6ci")
 
+class WhatsAppIntegration:
+    def get_schedules(self):
+        headers = {
+            "Authorization": f"Api-Key {BACKEND_API_KEY}"
+        }
 
-def get_schedules():
-    headers = {
-        "Authorization": f"Api-Key {BACKEND_API_KEY}"
-    }
+        response = requests.get(
+            f"{BACKEND_URL}/schedule/?has_sync=0",
+            headers=headers,
+        )
+        response_data = response.json()
+        return response_data
 
-    response = requests.get(
-        f"{BACKEND_URL}/schedule/?has_sync=0",
-        headers=headers,
-    )
-    response_data = response.json()
-    return response_data
+    def send_to_whatsapp(self, message):
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "chatId": f"{RECIPIENT_PHONE_NUMBER}@c.us",
+            "text": message,
+            "session": "default"
+        }
+        response = requests.post(
+            f"{LAB_ZAP_URL}/api/sendText",
+            json=payload,
+            headers=headers,
+        )
+        return response
 
+    def update_schedule(self, schedule_id):
+        headers = {
+            "Authorization": f"Api-Key {BACKEND_API_KEY}"
+        }
 
-def send_to_whatsapp(message):
-    headers = {"Content-Type": "application/json"}
-    payload = {
-        "chatId": f"{RECIPIENT_PHONE_NUMBER}@c.us",
-        "text": message,
-        "session": "default"
-    }
-    response = requests.post(
-        f"{LAB_ZAP_URL}/api/sendText",
-        json=payload,
-        headers=headers,
-    )
-    return response
-
-def update_schedule(schedule_id):
-    headers = {
-        "Authorization": f"Api-Key {BACKEND_API_KEY}"
-    }
-
-    response = requests.patch(
-        f"{BACKEND_URL}/schedule/{schedule_id}/",
-        {"has_sync": True},
-        headers=headers,
-    )
-    response_data = response.json()
-    return response_data
+        response = requests.patch(
+            f"{BACKEND_URL}/schedule/{schedule_id}/",
+            {"has_sync": True},
+            headers=headers,
+        )
+        response_data = response.json()
+        return response_data
 
 
 def main():
+    wpp = WhatsAppIntegration()
     try:
         while True:
-            schedules = get_schedules()
-            for schedule in schedules:
-                send_to_whatsapp(schedule["message"])
+            schedules = wpp.get_schedules()
 
-                update_schedule(schedule["id"])
+            for schedule in schedules:
+                wpp.send_to_whatsapp(schedule["whatsapp_message"])
+                wpp.update_schedule(schedule["id"])
 
             sleep(WAIT_TIME)
     except KeyboardInterrupt:
