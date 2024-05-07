76d2a9b2c6a255a1409b57f843dc9fbf0650e6ab
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-05-07 00:29:06 -0300

    feat: implements django filters

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework import generics, mixins
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from core.models import Schedule
 from core.serializers import ScheduleSerializer
 
@@ -7,3 +7,5 @@ from core.serializers import ScheduleSerializer
 class ScheduleViewSet(ModelViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["has_sync"]
--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "rest_framework_api_key",
+    "django_filters",
     "core",
 ]
 
