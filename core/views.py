40df5df235d06fd80e1e04114508ea46d6b99841
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-05-07 09:50:03 -0300

    feat: call sync to google service on schedule view

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
+import multiprocessing
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from core.models import Schedule
 from core.serializers import ScheduleSerializer
 from rest_framework_api_key.permissions import HasAPIKey
 from rest_framework_jwt.permissions import IsSuperUser
+from core.services import CalendarService
 
 
 class ScheduleViewSet(ModelViewSet):
@@ -12,3 +17,19 @@ class ScheduleViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["has_sync"]
     permission_classes = [HasAPIKey | IsSuperUser]
+
+    def perform_create(self, serializer: ScheduleSerializer):
+        instance: Schedule = serializer.save()
+        self._sync_to_google_calendar(instance)
+
+    def perform_update(self, serializer: ScheduleSerializer):
+        instance: Schedule = serializer.save()
+        self._sync_to_google_calendar(instance)
+
+    def _sync_to_google_calendar(self, instance: Schedule):
+        process = multiprocessing.Process(
+            target=CalendarService.sync_to_google_api,
+            args=[instance],
+        )
+        process.start()
+        return
