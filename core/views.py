100b9c85ff49342f70c4e3929c13586a50fba3df
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-05-07 01:25:35 -0300

    feat: change schedule views permission classes

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from core.models import Schedule
 from core.serializers import ScheduleSerializer
 from rest_framework_api_key.permissions import HasAPIKey
+from rest_framework_jwt.permissions import IsSuperUser
 
 
 class ScheduleViewSet(ModelViewSet):
@@ -10,4 +11,4 @@ class ScheduleViewSet(ModelViewSet):
     serializer_class = ScheduleSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["has_sync"]
-    permission_classes = [HasAPIKey]
+    permission_classes = [HasAPIKey | IsSuperUser]
