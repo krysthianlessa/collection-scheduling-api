aec742e516643b7196ade67aa7e4ea3b5536d96c
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-05-09 11:20:46 -0300

    feat: remove async method when integrate with google api

--- core/signals.py
+++ core/signals.py
@@ -14,8 +14,9 @@ def deactivate_shared_calendars(sender, instance: SharedCalendar, **kwargs):
 
 @receiver(post_save, sender=Schedule)
 def sync_google_calendar(sender, instance: Schedule, **kwargs):
-    process = multiprocessing.Process(
-        target=CalendarService.sync_to_google_api,
-        args=[instance],
-    )
-    process.start()
+    # process = multiprocessing.Process(
+    #     target=CalendarService.sync_to_google_api,
+    #     args=[instance],
+    # )
+    # process.start()
+    CalendarService.sync_to_google_api(instance)
